216b8986499a9f1d9f45023ee1eab0bdd4fc3be5
itsmanishsingh <itsmanishsingh6@gmail.com>
2022-12-15 18:49:52 +0530

    Project Initiate

---
+++ script.js
@@ -0,0 +1,15 @@
+let userinput ;
+function insert() {
+  userinput = document.getElementsByClassName('button');
+  
+  userinputval = userinput.innerHTML;
+  // userinput = userinput*10+value;
+  console.log(userinput);
+
+}
+
+function equals() {}
+
+function clean() {}
+
+function back() {}
